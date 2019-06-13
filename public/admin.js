a4e6f462f2186da7039e3cbdee8dc09eb910b94b
Daniel Scott <jdscottcampos@gmail.com>
2019-06-12 21:23:16 -0600

    got main tutorial working

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,31 @@
 var app = new Vue({
   el: '#admin',
-  data: {},
-  methods: {}
+  data: {
+    title: "",
+    selected:  "",
+    addItem: null,
+    photos: [
+      {name: 'baseball', id: 1, path: '/images/baseball.jpg'},
+      {name: 'car', id: 2, path: '/images/car.jpg'},
+      {name: 'glasses', id: 3, path: '/images/glasses.jpg'},
+      {name: 'paintbrush', id: 4, path: '/images/paintbrush.jpg'},
+      {name: 'pen', id: 5, path: '/images/pen.jpg'},
+      {name: 'scissors', id: 6, path: '/images/scissors.jpg'},
+      {name: 'shovel', id: 7, path: '/images/shovel.jpg'},
+      {name: 'slinky', id: 8, path: '/images/slinky.jpg'},
+    ],
+  },
+  methods: {
+    async addNewItem(){
+      try {
+        let result = await axios.post('/api/items', {
+          title: this.title,
+          path: this.selected.path
+        });
+        this.addItem = result.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 });
